c169efed75c0ecc33e2f62983dffec414110268f
fdcote <48247604+fdcote@users.noreply.github.com>
2025-10-29 09:21:21 -0400

    feat: XDG history location

--- src/services/History.zig
+++ src/services/History.zig
@@ -6,6 +6,7 @@ allocator: std.mem.Allocator,
 config: *Config,
 items: std.ArrayList([]const u8),
 index: isize,
+path: []u8,
 
 pub fn init(allocator: std.mem.Allocator, config: *Config) Self {
     var self = Self{
@@ -13,6 +14,7 @@ pub fn init(allocator: std.mem.Allocator, config: *Config) Self {
         .config = config,
         .items = .{},
         .index = -1,
+        .path = "",
     };
 
     if (config.general.history <= 0) return self;
@@ -20,16 +22,19 @@ pub fn init(allocator: std.mem.Allocator, config: *Config) Self {
     const home = std.process.getEnvVarOwned(allocator, "HOME") catch return self;
     defer allocator.free(home);
 
-    var history_path_buf: [std.fs.max_path_bytes]u8 = undefined;
-    const history_path = std.fmt.bufPrint(&history_path_buf, "{s}/.local/state/fancy-cat/history.txt", .{home}) catch return self;
+    if (!config.legacy_path) {
+        const xdg_state_home = std.process.getEnvVarOwned(allocator, "XDG_STATE_HOME") catch null;
+        if (xdg_state_home) |x| {
+            self.path = std.fmt.allocPrint(allocator, "{s}/fancy-cat/history", .{x}) catch return self;
+            allocator.free(x);
+        } else self.path = std.fmt.allocPrint(allocator, "{s}/.local/state/fancy-cat/history", .{home}) catch return self;
+    } else self.path = std.fmt.allocPrint(allocator, "{s}/.fancy-cat_history", .{home}) catch return self;
 
-    const file = std.fs.openFileAbsolute(history_path, .{ .mode = .read_only }) catch return self;
-    defer file.close();
+    const content = std.fs.cwd().readFileAlloc(allocator, self.path, 1024 * 1024) catch null;
+    if (content == null) return self;
+    defer allocator.free(content.?);
 
-    const content = file.readToEndAlloc(allocator, 1024 * 1024) catch return self;
-    defer allocator.free(content);
-
-    var line = std.mem.tokenizeScalar(u8, content, '\n');
+    var line = std.mem.tokenizeScalar(u8, content.?, '\n');
     while (line.next()) |cmd| {
         const cmd_copy = allocator.dupe(u8, cmd) catch continue;
         self.items.append(allocator, cmd_copy) catch {
@@ -42,29 +47,16 @@ pub fn init(allocator: std.mem.Allocator, config: *Config) Self {
 }
 
 pub fn deinit(self: *Self) void {
-    defer {
-        for (self.items.items) |entry| {
-            self.allocator.free(entry);
-        }
-        self.items.deinit(self.allocator);
-    }
-
     if (self.config.general.history <= 0) return;
 
-    const home = std.process.getEnvVarOwned(self.allocator, "HOME") catch return;
-    defer self.allocator.free(home);
+    defer {
+        for (self.items.items) |entry| self.allocator.free(entry);
+        self.items.deinit(self.allocator);
+        if (self.path.len > 0) self.allocator.free(self.path);
+    }
 
-    var history_dir_buf: [std.fs.max_path_bytes]u8 = undefined;
-    const history_dir = std.fmt.bufPrint(&history_dir_buf, "{s}/.local/state/fancy-cat", .{home}) catch return;
-
-    std.fs.makeDirAbsolute(history_dir) catch |err| {
-        if (err != error.PathAlreadyExists) return;
-    };
-
-    var history_path_buf: [std.fs.max_path_bytes]u8 = undefined;
-    const history_path = std.fmt.bufPrint(&history_path_buf, "{s}/history.txt", .{history_dir}) catch return;
-
-    const file = std.fs.createFileAbsolute(history_path, .{}) catch return;
+    if (std.fs.path.dirname(self.path)) |dir| std.fs.cwd().makePath(dir) catch {};
+    const file = std.fs.createFileAbsolute(self.path, .{}) catch return;
     defer file.close();
 
     for (self.items.items) |cmd| {
